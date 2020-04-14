9ce6cc97d6b8eb79c206b56d388164756d2cb812
sofdev-ms <msharmajiit@gmail.com>
2020-04-14 16:52:16 +0530

    used template engines and made routes

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bookRouter = require('./src/routes/bookRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 // app.use(morgan('combined'));
+
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,81 @@
+const express = require('express');
+
+const bookRouter = express.Router();
+
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render(
+      'books',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library',
+        books
+      }
+    );
+  });
+
+bookRouter.route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    res.render(
+      'book',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library',
+        book: books[id]
+      }
+    );
+  });
+module.exports = bookRouter;
